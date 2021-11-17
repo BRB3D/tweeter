e4c89a1864d036b64fb2e86051a701555013a668
BRB3D <samuraix221@hotmail.com>
2021-11-17 16:22:04 -0500

    hover animation for tweeting, client.js now iterates through hard coded database, articles eliminated from index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,23 +7,34 @@
 $(function() {
   console.log( "ready!" );
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const tweetData =[
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
- }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-  const $tweet = createTweetElement(tweetData);
 
-
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet);
+  /* console.log($tweet); // to see what it looks like */
+  renderTweets(tweetData, createTweetElement);
 });
 
 
@@ -40,7 +51,7 @@ const createTweetElement = function(tweetObj) {
       <p>${tweetObj.content.text}</p>
     </div>
     <footer>
-      <p>${tweetObj.created_at}</p>
+      <p>${timeago.format(tweetObj.created_at)}</p>
       <div class="icon">
         <i class="fab fa-canadian-maple-leaf"></i>
         <i class="fas fa-retweet"></i>
@@ -48,4 +59,11 @@ const createTweetElement = function(tweetObj) {
     </div>
     </footer>
   </article>`
+}
+
+const renderTweets = function(data, callback) {
+  for (let tweets of data) {
+    const $tweet = callback(tweets);
+    $('#tweets-container').append($tweet);
+  }
 }
